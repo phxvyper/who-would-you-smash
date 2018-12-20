2c068d0dd193b7ea8c880c45cc0df747f7ce24f8
Tristen Horton <tristen@tristenhorton.com>
2018-12-20 13:10:07 -0700

    remove save feature and excess code

--- index.js
+++ index.js
@@ -96,29 +96,6 @@ const characters = [
     new Character(70, "Piranha Plant", "packun-flower")
 ];
 
-const serializedData = {
-    "would": "",
-    "maybe": "",
-    "wouldnt": "",
-    "start-list": "",
-};
-
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-function drag(ev) {
-    ev.dataTransfer.setData("text/plain", ev.target.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    const data = ev.dataTransfer.getData("text/plain");
-    if (data) {
-        ev.target.appendChild(document.getElementById(data));
-    }
-}
-
 function editableTextBlurred() {
     const html = $(this).val();
     const viewableText = $("<div class='row'>");
@@ -137,60 +114,14 @@ function makeDivEditable() {
     editableText.blur(editableTextBlurred);
 }
 
-function escapeSection(str) {
-    return str.replaceAll('\\&', 'a')
-        .replaceAll('\\[', 'l')
-        .replaceAll('\\]', 'r')
-        .replaceAll('\\=', 'e')
-        .replaceAll('\\.', 'd') + "s";
-}
-
-function addSection(section, element) {
-    section.split("a").forEach(fighter => {
-        $(`#f-${fighter.split("e")[1]}`).appendTo(element);
-    });
-}
-
-function updateUri() {
-    let hash = "";
-
-    hash += escapeSection(serializedData["would"]);
-    hash += escapeSection(serializedData["maybe"]);
-    hash += escapeSection(serializedData["wouldnt"]);
-    hash += escapeSection(serializedData["start-list"]);
-
-    while (hash.length % 4 != 0) {
-        hash += "p";
-    }
-
-    hash = window.atob(hash);
-    window.location.hash = hash;
-}
-
 $(function() {
     const startList = $("#start-list");
     characters.forEach(element => {
         $(`<li id="f-${element.number}"><img class="fighter" src="${element.image}" alt="${element.number}: ${element.name}"></li>`).appendTo(startList);
     });
 
-    if (window.location.hash.length > 1) {
-        const decoded = decodeURIComponent(window.location.hash.substring(1));
-        const unhashed = btoa(decoded);
-        const sections = unhashed.replaceAll("p", "").split("s");
-
-        addSection(sections[0], "#would");
-        addSection(sections[1], "#maybe");
-        addSection(sections[2], "#wouldnt");
-        addSection(sections[3], "#start-list");
-    }
-
     $(".connected-sortable").sortable({
-        connectWith: ".connected-sortable",
-        update: function(event, ui) {
-            const data = $(this).sortable("serialize");
-            serializedData[this.id] = data;
-            updateUri();
-        }
+        connectWith: ".connected-sortable"
     }).disableSelection();
 
     $("div.row").click(makeDivEditable);
