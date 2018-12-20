be3ddf63f9154ce129c0db010258a6bea9653c1a
Tristen Horton <tristen@tristenhorton.com>
2018-12-19 22:53:51 -0700

    fixed name

---
+++ index.js
@@ -0,0 +1,144 @@
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+
+class Character {
+    constructor(number, name, codename) {
+        this.number = number;
+        this.name = name;
+        if (codename) {
+            this.image = codename;
+        }
+        else {
+            this.image = name.toLowerCase()
+                .replaceAll('[\\s\\-]', '_')
+                .replaceAll('&', 'and')
+                .replaceAll('\\.', '');
+        }
+        this.image = `https://www.smashbros.com/assets_v2/img/fighter/thumb_h/${this.image}.png`;
+    }
+}
+
+const characters = [
+    new Character(1, "Mario"),
+    new Character(2, "Donkey Kong"),
+    new Character(3, "Link"),
+    new Character(4, "Samus"),
+    new Character(4.1, "Dark Samus"),
+    new Character(5, "Yoshi"),
+    new Character(6, "Kirby"),
+    new Character(7, "Fox"),
+    new Character(8, "Pikachu"),
+    new Character(9, "Luigi"),
+    new Character(10, "Ness"),
+    new Character(11, "Captain Falcon"),
+    new Character(12, "Jigglypuff"),
+    new Character(13, "Peach"),
+    new Character(13.1, "Daisy"),
+    new Character(14, "Bowser"),
+    new Character(15, "Ice Climbers"),
+    new Character(16, "Sheik"),
+    new Character(17, "Zelda"),
+    new Character(18, "Dr. Mario"),
+    new Character(19, "Pichu"),
+    new Character(20, "Falco"),
+    new Character(21, "Marth"),
+    new Character(21.1, "Lucina"),
+    new Character(22, "Young Link"),
+    new Character(23, "Ganondorf"),
+    new Character(24, "Mewtwo"),
+    new Character(25, "Roy"),
+    new Character(25.1, "Chrom"),
+    new Character(26, "Mr. Game & Watch"),
+    new Character(27, "Meta Knight"),
+    new Character(28, "Pit"),
+    new Character(28.1, "Dark Pit"),
+    new Character(29, "Zero Suit Samus"),
+    new Character(30, "Wario"),
+    new Character(31, "Snake"),
+    new Character(32, "Ike"),
+    new Character(33, "Pokemon Trainer"),
+    new Character(36, "Diddy Kong"),
+    new Character(37, "Lucas"),
+    new Character(38, "Sonic"),
+    new Character(39, "King Dedede"),
+    new Character(40, "Olimar"),
+    new Character(41, "Lucario"),
+    new Character(42, "R.O.B."),
+    new Character(43, "Toon Link"),
+    new Character(44, "Wolf"),
+    new Character(45, "Villager"),
+    new Character(46, "Mega Man"),
+    new Character(47, "Wii Fit Trainer"),
+    new Character(48, "Rosalina & Luma"),
+    new Character(49, "Little Mac"),
+    new Character(59, "Greninja"),
+    new Character(51, "Mii Fighter"),
+    new Character(54, "Palutena"),
+    new Character(55, "Pac-Man"),
+    new Character(56, "Robin"),
+    new Character(57, "Shulk"),
+    new Character(58, "Bowser Jr."),
+    new Character(59, "Duck Hunt"),
+    new Character(60, "Ryu"),
+    new Character(60.1, "Ryu"),
+    new Character(61, "Cloud"),
+    new Character(62, "Corrin"),
+    new Character(63, "Bayonetta"),
+    new Character(64, "Inkling"),
+    new Character(65, "Ridley"),
+    new Character(66, "Simon"),
+    new Character(66.1, "Richter"),
+    new Character(67, "King K. Rool"),
+    new Character(68, "Isabelle", "shizue"),
+    new Character(69, "Incineroar", "gaogaen"),
+    new Character(70, "Piranha Plant", "packun_flower")
+];
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text/plain", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    const data = ev.dataTransfer.getData("text/plain");
+    if (data) {
+        ev.target.appendChild(document.getElementById(data));
+    }
+}
+
+function editableTextBlurred() {
+    const html = $(this).val();
+    const viewableText = $("<div class='row'>");
+    viewableText.html(html);
+    $(this).replaceWith(viewableText);
+
+    $(viewableText).click(makeDivEditable);
+}
+
+function makeDivEditable() {
+    const divHtml = $(this).html();
+    const editableText = $("<textarea />");
+    editableText.val(divHtml);
+    $(this).replaceWith(editableText);
+    editableText.focus();
+    editableText.blur(editableTextBlurred);
+}
+
+$(function() {
+    const startList = $("#start-list");
+    characters.forEach(element => {
+        $(`<li><img src="${element.image}" id="fighter-${element.number}" class="fighter" alt="${element.number}: ${element.name}"></li>`).appendTo(startList);
+    });
+
+    $(".connected-sortable").sortable({
+        connectWith: ".connected-sortable"
+    }).disableSelection();
+
+    $("div.row").click(makeDivEditable);
+});
